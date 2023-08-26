771f067da4de69a95e4b39f815389632de7ef495
Narmeen alkatab <narmeen.kattab1@gmail.com>
2023-08-26 18:09:32 +0300

    39

---
+++ pages/login.js
@@ -0,0 +1,23 @@
+import MyHead from "../components/Head";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import LoginForm from "@/components/LoginForm";
+import { Inter } from "next/font/google";
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Login() {
+  return (
+    <>
+      <MyHead data={"Login"} />
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <main
+          className={`flex flex-col items-center justify-between p-20 ${inter.className}`}
+        >
+          <LoginForm />
+        </main>
+        <Footer />
+      </div>
+    </>
+  );
+}
